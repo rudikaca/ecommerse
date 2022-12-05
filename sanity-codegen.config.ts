5e1e05f26a0663fa7288d5781d0262c82324914c
Rudin Kaca <kacarudi@gmail.com>
2022-12-05 22:25:33 +0100

    types configuration

---
+++ sanity-codegen.config.ts
@@ -0,0 +1,8 @@
+import { SanityCodegenConfig } from 'sanity-codegen';
+
+const config: SanityCodegenConfig = {
+    schemaPath: './sanity_ecommerce/schemas/schema.js',
+    outputPath: './schema.ts',
+};
+
+export default config;
