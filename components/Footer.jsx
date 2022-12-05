281da20ac0dc922453f57b87e02ed9204e6d5e95
Rudin Kaca <kacarudi@gmail.com>
2022-12-05 22:57:54 +0100

    Navbar and Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <>
-            Footer
-        </>
+        <div className="footer-container">
+            <p>2022 JSM Headphones All rights reserved</p>
+            <p className="icons">
+                <AiFillInstagram />
+                <AiOutlineTwitter />
+            </p>
+        </div>
     )
 }
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const Navbar = () => {
+    const totalQuantities = 1;
     return (
-        <>
-            Navbar
-        </>
+        <div className="navbar-container">
+            <p className="logo">
+                <Link href="/">JSM Headphones</Link>
+            </p>
+
+            <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
+                <AiOutlineShopping />
+                <span className="cart-item-qty">{totalQuantities}</span>
+            </button>
+        </div>
     )
 }
 
-export default Navbar;
+export default Navbar
